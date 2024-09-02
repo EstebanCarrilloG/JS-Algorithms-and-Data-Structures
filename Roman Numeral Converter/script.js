55bac42848742065a6f0b47d2830990b212a038e
Esteban Carrillo <estexav0996@gmail.com>
2024-09-02 13:40:07 -0500

    fixing bugs , adding styles

--- Roman Numeral Converter/script.js
+++ Roman Numeral Converter/script.js
@@ -4,14 +4,20 @@ const output = document.getElementById("output");
 
 function validateInput(){
   let numberValue = number.value;
+
+  output.removeAttribute("hidden");
   if(numberValue ==""){
+    output.classList.add("error");
     output.innerText ="Please enter a valid number";
 
   } else if(numberValue <=0){
+    output.classList.add("error");
     output.innerText ="Please enter a number greater than or equal to 1";
   }else if(numberValue >= 4000){
+    output.classList.add("error");
     output.innerText ="Please enter a number less than or equal to 3999";
   }else{
+    output.classList.remove("error");
     output.innerText = convertNumber(numberValue);
   }
 }
@@ -41,11 +47,11 @@ const numbers = [
 
 
 let miles=(number%10000-number%1000)/1000;
-let centenas=(number%1000-number%100)/100;
-let decenas=(number%100-number%10)/10;
-let unidades=number%10;
+let hundreds=(number%1000-number%100)/100;
+let tens=(number%100-number%10)/10;
+let units=number%10;
 
-console.log(unidades)
+console.log(units)
 
 
 if(numbers[0][number]){
@@ -55,92 +61,92 @@ if(numbers[0][number]){
 if(number < 4){
   return numbers[0][number];
 }else if(number < 10){
-  return numbers[0][5] + numbers[0][unidades]; 
+  return numbers[0][5] + numbers[0][units]; 
 }else if(number < 40){
-  return numbers[0][10].repeat(number / 10) + (unidades === 0 ? "":numbers[0][unidades]);
+  return numbers[0][10].repeat(number / 10) + (units === 0 ? "":numbers[0][units]);
 }else if (number < 50){
-  return numbers[0][40] + numbers[0][unidades];
+  return numbers[0][40] + numbers[0][units];
 }else if (number < 90){
-  return numbers[0][50] + (unidades === 0 ? numbers[0][10].repeat(decenas - 5): numbers[0][10].repeat(decenas - 5) + numbers[0][unidades]);
+  return numbers[0][50] + (units === 0 ? numbers[0][10].repeat(tens - 5): numbers[0][10].repeat(tens - 5) + numbers[0][units]);
 }else if (number < 100){
-  return numbers[0][90] + numbers[0][unidades];
+  return numbers[0][90] + numbers[0][units];
 }else if (number < 400){
-  return numbers[0][100] + (
-  ((decenas * 10 + unidades) < 10) 
-    ? numbers[0][unidades]
-  :((decenas * 10 + unidades)  < 40) 
-    ? (numbers[0][10].repeat(decenas / 1)) + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 50) 
-    ?  numbers[0][40] + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 90) 
-    ? numbers[0][50] + (unidades === 0 ? numbers[0][10].repeat(decenas - 5): numbers[0][10].repeat(decenas - 5) + numbers[0][unidades])
-   :((decenas * 10 + unidades)  < 100)
-    ? numbers[0][90] + numbers[0][unidades]
+  return numbers[0][100].repeat(number / 100) + (units === 0 && tens === 0 ? "":
+  ((tens * 10 + units) < 10) 
+    ? numbers[0][units]
+  :((tens * 10 + units)  < 40) 
+    ? (numbers[0][10].repeat(tens / 1)) + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 50) 
+    ?  numbers[0][40] + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 90) 
+    ? numbers[0][50] + (units === 0 ? numbers[0][10].repeat(tens - 5): numbers[0][10].repeat(tens - 5) + numbers[0][units])
+   :((tens * 10 + units)  < 100)
+    ? numbers[0][90] + numbers[0][units]
   :0
   )
 }
 else if(number < 500){
   return numbers[0][400] + (
-  ((decenas * 10 + unidades) < 10) 
-    ? numbers[0][unidades]
-  :((decenas * 10 + unidades)  < 40) 
-    ? (numbers[0][10].repeat(decenas / 1)) + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 50) 
-    ?  numbers[0][40] + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 90) 
-    ? numbers[0][50] + (unidades === 0 ? numbers[0][10].repeat(decenas - 5): numbers[0][10].repeat(decenas - 5) + numbers[0][unidades])
-   :((decenas * 10 + unidades)  < 100)
-    ? numbers[0][90] + numbers[0][unidades]
+  ((tens * 10 + units) < 10) 
+    ? numbers[0][units]
+  :((tens * 10 + units)  < 40) 
+    ? (numbers[0][10].repeat(tens / 1)) + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 50) 
+    ?  numbers[0][40] + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 90) 
+    ? numbers[0][50] + (units === 0 ? numbers[0][10].repeat(tens - 5): numbers[0][10].repeat(tens - 5) + numbers[0][units])
+   :((tens * 10 + units)  < 100)
+    ? numbers[0][90] + numbers[0][units]
   :0
   )
 
 }
 
 else if(number < 900){
-  return (numbers[0][500] +numbers[0][100].repeat(centenas - 5)) + (
-  ((decenas * 10 + unidades) < 10) 
-    ? numbers[0][unidades]
-  :((decenas * 10 + unidades)  < 40) 
-    ? (numbers[0][10].repeat(decenas / 1)) + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 50) 
-    ?  numbers[0][40] + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 90) 
-    ? numbers[0][50] + (unidades === 0 ? numbers[0][10].repeat(decenas - 5): numbers[0][10].repeat(decenas - 5) + numbers[0][unidades])
-   :((decenas * 10 + unidades)  < 100)
-    ? numbers[0][90] + numbers[0][unidades]
+  return (numbers[0][500] +numbers[0][100].repeat(hundreds - 5)) + (units === 0 && tens === 0 ? "" :
+  ((tens * 10 + units) < 10) 
+    ? numbers[0][units]
+  :((tens * 10 + units)  < 40) 
+    ? (numbers[0][10].repeat(tens / 1)) + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 50) 
+    ?  numbers[0][40] + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 90) 
+    ? numbers[0][50] + (units === 0 ? numbers[0][10].repeat(tens - 5): numbers[0][10].repeat(tens - 5) + numbers[0][units])
+   :((tens * 10 + units)  < 100)
+    ? numbers[0][90] + numbers[0][units]
   :0
   )
 
 }
 else if(number < 1000){
   return (numbers[0][900]) + (
-  ((decenas * 10 + unidades) < 10) 
-    ? numbers[0][unidades]
-  :((decenas * 10 + unidades)  < 40) 
-    ? (numbers[0][10].repeat(decenas / 1)) + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 50) 
-    ?  numbers[0][40] + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 90) 
-    ? numbers[0][50] + (unidades === 0 ? numbers[0][10].repeat(decenas - 5): numbers[0][10].repeat(decenas - 5) + numbers[0][unidades])
-   :((decenas * 10 + unidades)  < 100)
-    ? numbers[0][90] + numbers[0][unidades]
+  ((tens * 10 + units) < 10) 
+    ? numbers[0][units]
+  :((tens * 10 + units)  < 40) 
+    ? (numbers[0][10].repeat(tens / 1)) + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 50) 
+    ?  numbers[0][40] + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 90) 
+    ? numbers[0][50] + (units === 0 ? numbers[0][10].repeat(tens - 5): numbers[0][10].repeat(tens - 5) + numbers[0][units])
+   :((tens * 10 + units)  < 100)
+    ? numbers[0][90] + numbers[0][units]
   :0
   )
 
 }
 
 else if(number < 4000){
-  return (numbers[0][1000].repeat(miles) )  + (centenas * 100 < 400 ? numbers[0][100].repeat(centenas):centenas * 100 < 500 ?numbers[0][400]: centenas * 100 < 900 ? numbers[0][500] + numbers[0][100].repeat(centenas - 5):centenas * 100 < 1000 ? numbers[0][900]  : 0 ) + (
-  ((decenas * 10 + unidades) < 10) 
-    ? numbers[0][unidades]
-  :((decenas * 10 + unidades)  < 40) 
-    ? (numbers[0][10].repeat(decenas / 1)) + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 50) 
-    ?  numbers[0][40] + (unidades == 0 ? "":numbers[0][unidades])
-  :((decenas * 10 + unidades)  < 90) 
-    ? numbers[0][50] + (unidades === 0 ? numbers[0][10].repeat(decenas - 5): numbers[0][10].repeat(decenas - 5) + numbers[0][unidades])
-   :((decenas * 10 + unidades)  < 100)
-    ? numbers[0][90] + numbers[0][unidades]
+  return (numbers[0][1000].repeat(miles) )  + ( hundreds * 100 < 400 ?  numbers[0][100].repeat(hundreds):hundreds * 100 < 500 ?numbers[0][400]: hundreds * 100 < 900 ? numbers[0][500] + numbers[0][100].repeat(hundreds - 5):hundreds * 100 < 1000 ? numbers[0][900]  : 0 ) + (units === 0 && tens === 0 ? "" :
+  ((tens * 10 + units) < 10) 
+    ? numbers[0][units]
+  :((tens * 10 + units)  < 40) 
+    ? (numbers[0][10].repeat(tens / 1)) + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 50) 
+    ?  numbers[0][40] + (units == 0 ? "":numbers[0][units])
+  :((tens * 10 + units)  < 90) 
+    ? numbers[0][50] + (units === 0 ? numbers[0][10].repeat(tens - 5): numbers[0][10].repeat(tens - 5) + numbers[0][units])
+   :((tens * 10 + units)  < 100)
+    ? numbers[0][90] + numbers[0][units]
   :0
   )
 
